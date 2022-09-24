706a52009c46ec62fd89142da45e1e87884f5ab5
rtwillett <ryan.willett@gmail.com>
2022-09-24 17:34:47 -0400

    Adding a more templates and dashboards

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, url_for, request, session, flash, redirect
+from ocr import ocr
+from ner import ner
+from summarization import summ
 
 # Importing all of the Blueprint objects into the application
 from flask_wtf.csrf import CSRFProtect
@@ -15,6 +18,11 @@ app.config['UPLOAD_FOLDER'] = './uploads'
 app.config['DATA_FOLDER'] = './application_data'
 
 csrf = CSRFProtect(app)
+
+# Registering all Blueprints (makes them available application)
+app.register_blueprint(ner, url_prefix="")
+app.register_blueprint(ocr, url_prefix="")
+app.register_blueprint(summ, url_prefix="")
 
 # Routing
 @app.route("/")
