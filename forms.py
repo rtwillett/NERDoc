de385cf2385533728354926c4fe74d5c8a037678
rtwillett <ryan.willett@gmail.com>
2022-09-25 10:18:24 -0400

    Env issues resolved

--- forms.py
+++ forms.py
@@ -2,10 +2,10 @@ import string
 from tokenize import String
 # from tkinter.tix import Form
 # from sqlalchemy import Integer
-from wtforms.fields.choices import SelectField
+# from wtforms.fields.choices import SelectField
 from wtforms import BooleanField, StringField, PasswordField, FloatField, SubmitField, DateField, SelectField, RadioField, widgets, IntegerField, FieldList, FormField, FileField
-from wtforms.fields import choices
-from wtforms.fields.datetime import DateTimeLocalField
+# from wtforms.fields import choices
+# from wtforms.fields.datetime import DateTimeLocalField
 from wtforms.validators import DataRequired, Length, Email, NumberRange
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
